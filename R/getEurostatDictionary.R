d3d028c2852f6759ba5f0ad81ad4813dc669a3ac
Janne Huovari <janne.huovari@ptt.fi>
2014-07-04 14:15:26 +0300

    Fix getEurostatDictionary() failure to read table_dic

--- R/getEurostatDictionary.R
+++ R/getEurostatDictionary.R
@@ -25,5 +25,6 @@
 
 getEurostatDictionary <-
 	function(dictname) {
-  read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""), sep="\t", header=F, stringsAsFactors=FALSE)
+  read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""), 
+             sep="\t", header=F, stringsAsFactors=FALSE, quote = "\"")
 }
