bdd056d4fa8a1b28fea3dc3f6284e953e7697030
Lei <lei@folio>
2014-07-08 00:02:02 +0300

    added jhuovari fix

--- R/getEurostatDictionary.R
+++ R/getEurostatDictionary.R
@@ -24,6 +24,7 @@
 #' @keywords utilities database
 
 getEurostatDictionary <-
-	function(dictname) {
-  read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""), sep="\t", header=F, stringsAsFactors=FALSE)
+function(dictname) {
+  read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""),
+             sep="\t", header=F, stringsAsFactors=FALSE, quote = "\"")
 }
--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,5 +1,5 @@
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check --as-cran eurostat_0.9.31.tar.gz
-/usr/bin/R CMD INSTALL eurostat_0.9.31.tar.gz
+/usr/bin/R CMD check --as-cran eurostat_0.9.33.tar.gz
+/usr/bin/R CMD INSTALL eurostat_0.9.33.tar.gz
 
