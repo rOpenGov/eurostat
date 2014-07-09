3987b3e48149e5dcf7e5bfc1b0b5f885fb3a1a81
Janne Huovari <janne.huovari@ptt.fi>
2014-07-09 15:56:54 +0300

    First working version on label_eurostat

--- R/label_eurostat.R
+++ R/label_eurostat.R
@@ -1,20 +1,54 @@
 #' Get definitions for Eurostat codes
 #' 
+#' Get definitions for Eurostat codes from Eurostat dictionaries
 #' 
+#' A string or a factor vector of codes returns  a corresponding 
+#' vector of definations. For vectors a dictorionay name have to be supplied.
+#' For data.frames dictonary names are taken from column names. 
+#' "time" and "value" columns are returned as they were, so you can supply 
+#' data.frame from \code{\link{getEurostatRCV}} and get data.frame with 
+#' definations instead of codes.
+#' 
+#' @param x a vector or a data.frame. 
+#' @param dic a string (vector) naming eurostat dictionary or dictionaries.
+#'  If \code{NULL} (default) dictionry names taken from column names of the data.frame. 
+#' @import plyr
+#' @export
+#' @author Janne Huovari <janne.huovari@ptt.fi>
+#' @return a vector or a data.frame.
+#' @examples
+#' donotrun{
+#' lp <- getEurostatRCV("nama_aux_lp")
+#' lpl <- label_eurostat_vars(lp)
+#' str(lpl)
+#' label_eurostat_vars(names(lp))
+#' label_eurostat_tables("nama_aux_lp")
+#' }
 
-label_eurostat <- function(x, dic){
-  dic_df <- getEurostatDictionary(dic)
-  # in case of column names (not factors), change to upper case
-  if (!is.factor(x)) x <- toupper(x)
-  y <- plyr:::mapvalues(x, dic_df[,1], dic_df[,2], warn_missing = FALSE)
+label_eurostat <- function(x, dic = NULL){
+  if (is.data.frame(x)){
+    y <- x
+    for (i in names(y)[!(names(y) %in% c("time", "value"))]){
+      y[[i]] <- label_eurostat(y[[i]], i)
+    }
+  } else {
+    if (is.null(dic)) stop("Dictionary information is missing")
+    dic_df <- getEurostatDictionary(dic)
+    # in case of column names (not factors), change to upper case
+    if (!is.factor(x)) x <- toupper(x)
+    y <- mapvalues(x, dic_df[,1], dic_df[,2], warn_missing = FALSE)
+  }
   y
 }
-
-label_eurostat_df <- function(x){
-  if (!is.data.frame(x)) stop("x is not data.frame")
   
-  y <- as.data.frame(lapply(x[, !(names(x) %in% c("time", "value"))], label_eurostat))
-  y
+#' @describeIn label_eurostat Get definations for variable (column) names
+label_eurostat_vars <- function(x){
+  label_eurostat(x, dic = "dimlst")
 }
 
-lpl <- label_eurostat_df(lp)
+#' @describeIn label_eurostat Get definations for table names
+label_eurostat_tables <- function(x){
+  label_eurostat(x, dic = "table_dic")
+}
+
+
