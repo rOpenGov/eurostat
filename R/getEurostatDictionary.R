66e924d93ae58b42f8a6aeea10d4283237f3e9e4
Janne Huovari <janne.huovari@ptt.fi>
2014-07-10 10:41:34 +0300

    fix typo

--- R/getEurostatDictionary.R
+++ R/getEurostatDictionary.R
@@ -28,4 +28,3 @@ function(dictname) {
   read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""),
              sep="\t", header=F, stringsAsFactors=FALSE, quote = "\"")
 }
-
