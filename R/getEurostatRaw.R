63116bb71277cf9e2c4ec54aa2a5be6f9c88d7a1
Janne Huovari <janne.huovari@ptt.fi>
2014-08-07 23:08:29 +0300

    Simpler version of getEurostatRaw
    Now just download and turn to data.frame.

--- R/getEurostatRaw.R
+++ R/getEurostatRaw.R
@@ -2,43 +2,46 @@
 # All rights reserved.
 # This software is part of rOpenGov <ropengov.github.com>
 
-#' Download a dataset from the eurostat database (ec.europa.eu/eurostat).  
-#' 
-#' @description Download a dataset from the eurostat database. The dataset is transformed into the tabular format.
-#' 
-#' Arguments:
-#'  @param id A code name for the data set of interested. See the table of contents of eurostat datasets for more details.
+#'Download a dataset from the eurostat database (ec.europa.eu/eurostat).
 #'
-#' Returns:
-#'  @return A dataset in data.frame format. First column contains names of cases. Column names usually corresponds to years.
-#'
-#' @export
-#' @seealso \code{\link{getEurostatTOC}}, \code{\link{getEurostatRaw}}.
-#' @details Data is downloaded from \code{http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing} website.
-#' @references see citation("eurostat"). 
-#' @author Przemyslaw Biecek, Leo Lahti \email{louhos@@googlegroups.com} and Janne Huovari \email{janne.huovari@@ptt.fi}
+#'@description Download a dataset from the eurostat database. The dataset is
+#'  transformed into the tabular format.
+#'  
+#'  Arguments:
+#'@param id A code name for the data set of interested. See the table of
+#'  contents of eurostat datasets for more details.
+#'  
+#'  Returns:
+#'@return A dataset in data.frame format. First column contains comma separated
+#'  codes of cases. Other columns usually corresponds to years and column names 
+#'  are years with preceding X. Data is in character format as it contains values
+#'  together with eurostat flags for data.
+#'  
+#'@export
+#'@seealso \code{\link{getEurostatTOC}}, \code{\link{get_eurostat}}.
+#'@details Data is downloaded from
+#'  \code{http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing}
+#'  website.
+#'@references see citation("eurostat").
+#'@author Przemyslaw Biecek, Leo Lahti \email{louhos@@googlegroups.com} and
+#'  Janne Huovari \email{janne.huovari@@ptt.fi}
 #' @examples \dontrun{
-#' 	       tmp <- getEurostatRaw("educ_iste")
+#' 	       tmp <- get_eurostat_raw("educ_iste")
 #' 	       head(tmp)
 #'	     }
-#' @keywords utilities database
+#'@keywords utilities database
 
-getEurostatRaw <-
-function(id = "educ_iste") {
+get_eurostat_raw <-
+function(id) {
 
   adres <- paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=data%2F",id,".tsv.gz",sep="")
   tfile <- tempfile()
-
+  on.exit(unlink(tfile))
+  
   #  download and read file
   download.file(adres, tfile)
-  dat <- read.table(gzfile(tfile), sep="\t", na.strings = ": ", header=F, stringsAsFactors=F)
-  unlink(tfile)
-  colnames(dat) <- as.character(dat[1,])
-  dat <- dat[-1,]
-
-  #  remove additional marks
-  for (i in 2:ncol(dat)) {
-    dat[,i] <- extract_numeric(dat[,i])
-  }
+  dat <- read.table(gzfile(tfile), sep="\t", na.strings = ": ", 
+                    header = TRUE, stringsAsFactors = FALSE)
+  
   dat
 }
