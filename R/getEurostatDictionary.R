0f01d601f5261896c212299de45785458a3cf053
Markus Kainu <markuskainu@gmail.com>
2014-08-26 00:53:37 +0300

    vignette updated and some fileEncoding set to windows-1252 in getEurostatDictionary to display native region names with special characters properly

--- R/getEurostatDictionary.R
+++ R/getEurostatDictionary.R
@@ -26,5 +26,5 @@
 getEurostatDictionary <-
 function(dictname) {
   read.table(paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?file=dic%2Fen%2F",dictname,".dic",sep=""),
-             sep="\t", header=F, stringsAsFactors=FALSE, quote = "\"")
+             sep="\t", header=F, stringsAsFactors=FALSE, quote = "\"",fileEncoding="Windows-1252")
 }
