82143bfe02ddfb47645ae6461b40c470274a559d
antagomir <leo.lahti@iki.fi>
2015-03-11 23:42:57 +0100

    reshape in deps

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,5 +1,5 @@
 #/usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check --as-cran eurostat_0.9.36.tar.gz
-/usr/bin/R CMD INSTALL eurostat_0.9.36.tar.gz
+/usr/bin/R CMD check --as-cran eurostat_0.9.99.tar.gz
+/usr/bin/R CMD INSTALL eurostat_0.9.99.tar.gz
 
