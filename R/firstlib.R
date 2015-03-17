a21f2cd80eb9ba9534766bd9293f54341dc6d341
antagomir <leo.lahti@iki.fi>
2015-03-17 23:18:21 +0100

    DESCRIPTION updated and welcome message removed

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,9 +1,4 @@
 
 .EurostatEnv <- new.env()
 
-.onAttach <- function(lib, pkg)
-{
 
-  packageStartupMessage("Eurostat R tools. Copyright (C) 2014-2015 Leo Lahti, Przemyslaw Biecek, Janne Huovari and Markus Kainu. This is free software from rOpenGov http://ropengov.github.io")
-
-}
