efbd6f8b721956475a1c4b8b5a36f81c00c06414
antagomir <leo.lahti@iki.fi>
2016-01-18 21:27:39 +0100

    preparing cran release

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,12 +1,7 @@
-#~/bin/R-3.2.0/bin/R CMD BATCH document.R
-#~/bin/R-3.2.0/bin/R CMD build ../../
-#~/bin/R-3.2.0/bin/R CMD check --as-cran eurostat_1.1.9001.tar.gz
-#~/bin/R-3.2.0/bin/R CMD INSTALL eurostat_1.0.9001.tar.gz
-
-#/usr/local/bin/R CMD BATCH document.R
-/usr/local/bin/R CMD build ../../ --no-build-vignettes
-/usr/local/bin/R CMD check --as-cran eurostat_1.1.9007.tar.gz --no-build-vignettes
-/usr/local/bin/R CMD INSTALL eurostat_1.1.9007.tar.gz
+#/usr/bin/R CMD BATCH document.R
+/usr/bin/R CMD build ../../ --no-build-vignettes
+/usr/bin/R CMD check --as-cran eurostat_1.2.11.tar.gz --no-build-vignettes
+/usr/bin/R CMD INSTALL eurostat_1.2.11.tar.gz
 
 
 
