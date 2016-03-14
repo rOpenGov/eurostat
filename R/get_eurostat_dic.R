7145e5d61fa52cbdff6f127514326f3a5c1d90ed
antagomir <leo.lahti@iki.fi>
2016-03-14 22:35:11 +0100

    Bug report fixed - thanks ! - closes #50

--- R/get_eurostat_dic.R
+++ R/get_eurostat_dic.R
@@ -1,20 +1,17 @@
-#' @title Download a Eurostat dictionary
-#' @description Download a Eurostat dictionary. Download a dictionary for
-#'              given coded variable from Eurostat (ec.europa.eu/eurostat).
-#'              Dictionaries link codes with human-readable labels.
-#'              To translate codes to labels with \code{\link{label_eurostat}}.
-#' @param dictname A character, dictionary for given variable name 
-#'        will be downloaded.
-#' @param lang A character, code for language. Available are "en" (default), 
-#'        "fr" and "de".
-#' @return A data.frame with two columns, first with code names and 
-#'          second with full names.
+#' @title Download an Eurostat dictionary
+#' @description Download a Eurostat dictionary for
+#'    given coded variable from Eurostat (ec.europa.eu/eurostat).
+#'    The dictionaries link codes with human-readable labels.
+#'    To translate codes to labels, use \code{\link{label_eurostat}}.
+#' @param dictname A character, dictionary for the variable to be downloaded.
+#' @param lang A character, language code. Options: "en" (default) / "fr" / "de".
+#' @return data.frame with two columns: code names and full names.
 #' @export
 #' @importFrom utils read.table
 #' @seealso \code{\link{label_eurostat}}, \code{\link{get_eurostat}}, 
 #'          \code{\link{search_eurostat}}.
 #' @references see citation("eurostat"). 
-#' @author Przemyslaw Biecek and Leo Lahti \email{ropengov-forum@@googlegroups.com}
+#' @author Przemyslaw Biecek and Leo Lahti \email{leo.lahti@@iki.fi}. Thanks to Wietse Dol for contributions. 
 #' @examples \dontrun{
 #' 	       tmp <- get_eurostat_dic("crop_pro")
 #' 	       head(tmp)
@@ -26,12 +23,10 @@ get_eurostat_dic <- function(dictname, lang = "en") {
   utils::read.table(paste0(url, 
    "estat-navtree-portlet-prod/BulkDownloadListing?file=dic%2F", lang, "%2F",
    dictname, ".dic"),
-   sep = "\t", header = FALSE, stringsAsFactors = FALSE, quote = "\"",
-   fileEncoding = "Windows-1252")
+   sep = "\t", header = FALSE,
+   colClasses = c('character','character'),
+   stringsAsFactors = FALSE,
+   quote = "\"",
+   fileEncoding = "Windows-1252")		 
 }
 
-#' @describeIn get_eurostat_dic Old deprecated version
-#' @export
-getEurostatDictionary <- function(dictname){
-  .Deprecated("get_eurostat_dic")
-}
