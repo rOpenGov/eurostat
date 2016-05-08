7240b84195aced7e6b9f365a48dd2d291f877e63
Markus Kainu <markuskainu@gmail.com>
2016-05-08 11:43:58 +0300

    functions documented and vignettes updated accordingly

--- R/cut_to_classes.R
+++ R/cut_to_classes.R
@@ -1,57 +1,22 @@
-<<<<<<< HEAD
 #' @title Cuts the values column into classes and polishes the labels
-#' @description 
-#' @param x A numeric vector \code{values} 
-#' @param n A string (vector) naming eurostat dictionary or dictionaries.
-#'  If \code{NULL} (default) dictionry names taken from column names of 
-#'  the data.frame.
-#' @param method For data.frames names of the column for which also code columns
-#'   should be retained. The suffix "_code" is added to code column names.  
-#' @param manual Logical. Should ...
-#' @param manual_breaks 
-#' @param decimals 
+#' @description Categorises a numeric vector into automatic or manually defined categories 
+#' and polishes the labels ready for used in mapping with \code{merge_with_geodata} function and ggplot2.
+#' @param x A numeric vector, eg. \code{values} variable in data returned by  \code{get_eurostat}
+#' @param n A numeric. number of classes/categories
+#' @param style Chosen style: one of "fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", or "jenks"
+#' @param manual Logical. If manual breaks are being used
+#' @param manual_breaks. Numeric vector with manual threshold values
+#' @param decimals Number of decimals to include with labels
+#' @param nodata_label String. Text label for NA category.
 #' @export
 #' @author Markus Kainu <markuskainu@@gmail.com>
-=======
-#' @title Cut a value column into classes and polishes the labels
-#' @description Get definitions for Eurostat codes from Eurostat dictionaries.
-#' A character or a factor vector of codes returns a corresponding vector of
-#' definitions. \code{label_eurostat} labels also data.frames
-#' from \code{\link{get_eurostat}}.
-#' For vectors a dictionary name have to be supplied.
-#' For data.frames dictonary names are taken from column names. 
-#' "time" and "values" columns are returned as they were, so you can supply 
-#' data.frame from \code{\link{get_eurostat}} and get data.frame with 
-#' definitions instead of codes.
-#' @param x A character or a factor vector or a data.frame. 
-#' @param dic A string (vector) naming eurostat dictionary or dictionaries.
-#'  If \code{NULL} (default) dictionry names taken from column names of 
-#'  the data.frame.
-#' @param code For data.frames names of the column for which also code columns
-#'   should be retained. The suffix "_code" is added to code column names.  
-#' @param eu_order Logical. Should Eurostat ordering used for label levels. 
-#'   Affects only factors.
-#' @param lang A character, code for language. Available are "en" (default), 
-#'        "fr" and "de".
-#' @export
-#' @author Janne Huovari <janne.huovari@@ptt.fi>
->>>>>>> 0a2e28c0fa0ba9750e49739c9a7998fef19be93d
-#' @return a vector or a data.frame.
+#' @return a factor.
 #' @examples
 #'  \dontrun{
 #'    lp <- get_eurostat("nama_aux_lp")
-<<<<<<< HEAD
-#'    lp$class <- cut_to_classes(lp$values, n=5, method="jenks",decimals=1)
-=======
-#'    lpl <- label_eurostat(lp)
-#'    str(lpl)
-#'    lpl_order <- label_eurostat(lp, eu_order = TRUE)
-#'    lpl_code <- label_eurostat(lp, code = "unit")
-#'    label_eurostat_vars(names(lp))
-#'    label_eurostat_tables("nama_aux_lp")
->>>>>>> 0a2e28c0fa0ba9750e49739c9a7998fef19be93d
+#'    lp$class <- cut_to_classes(lp$values, n=5, style="jenks",decimals=1)
 #'  }
-cut_to_classes <- function(x, n=5,method="jenks",manual=FALSE,manual_breaks = NULL,decimals=0) {
+cut_to_classes <- function(x, n=5,style="jenks",manual=FALSE,manual_breaks = NULL,decimals=0,nodata_label="No data") {
   
   library(stringr)
   library(classInt)
@@ -62,7 +27,7 @@ cut_to_classes <- function(x, n=5,method="jenks",manual=FALSE,manual_breaks = NU
                                      dig.lab=5)))
   } else {
     levs <- as.data.frame(levels(cut(x, 
-                                     breaks=data.frame(classIntervals(x,n=n,method=method)[2])[,1],
+                                     breaks=data.frame(classIntervals(x,n=n,style=style)[2])[,1],
                                      include.lowest=T,
                                      dig.lab=5)))
   } 
@@ -96,7 +61,7 @@ cut_to_classes <- function(x, n=5,method="jenks",manual=FALSE,manual_breaks = NU
   }
   y <- as.character(y)
   #if (is.na(y)) {
-  y[is.na(y)] <- "No Data"
-  y <- factor(y, levels=c("No Data",labs[1:n]))
+  y[is.na(y)] <- nodata_label
+  y <- factor(y, levels=c(nodata_label,labs[1:n]))
   y
 }
--- R/merge_with_geodata.R
+++ R/merge_with_geodata.R
@@ -1,67 +1,28 @@
-<<<<<<< HEAD
-#' @title Get preprocessed spatial data from CISGO and merge with 
-#' @description Get preprocessed data.frame 
-#' definitions for Eurostat codes from Eurostat dictionaries.
-=======
-#' @title Merge with preprocessed spatial data from CISGO
-#' @description Get definitions for Eurostat codes from Eurostat dictionaries.
->>>>>>> 0a2e28c0fa0ba9750e49739c9a7998fef19be93d
-#' A character or a factor vector of codes returns a corresponding vector of
-#' definitions. \code{label_eurostat} labels also data.frames
-#' from \code{\link{get_eurostat}}.
-#' For vectors a dictionary name have to be supplied.
-#' For data.frames dictonary names are taken from column names. 
-#' "time" and "values" columns are returned as they were, so you can supply 
-#' data.frame from \code{\link{get_eurostat}} and get data.frame with 
-#' definitions instead of codes.
-<<<<<<< HEAD
-#' @param data A character or a factor vector or a data.frame. 
-#' @param geocol A string (vector) naming eurostat dictionary or dictionaries.
-#'  If \code{NULL} (default) dictionry names taken from column names of 
-#'  the data.frame.
-#' @param res For data.frames names of the column for which also code columns
-#'   should be retained. The suffix "_code" is added to code column names.  
+#' @title Merge preprocessed geospatial data from CISGO with data.frame from Eurostat
+#' @description Merges data.frame obtained from Eurostat with \code{get_eurostat} 
+#' with geospatial data preprocessed using \code{ggplot2::fortify}. Resulting data.frame can be plotted using ggplot2
+#' @param data A data.frame including a character vector that
+#' consists of values following current NUTS classification
+#' @param geocolumn A string. Name of the column with NUTS information 
+#' (\code{geo} in data.frames fetched using \code{get_eurostat})
+#' @param resolution Resolution of the geospatial data. One of "60" (1:60million), "20" (1:20million), "10" (1:10million), "01" (1:1million),
 #' @export
 #' @author Markus Kainu <markuskainu@@gmail.com>
 #' @return a data.frame.
 #' @examples
 #'  \dontrun{
 #'    lp <- get_eurostat("nama_aux_lp")
-#'    lpl <- merge_with_geodata(lp, geocol="geo", res=60)
+#'    lpl <- merge_with_geodata(lp, geocolumn="geo", resolution=60)
 #'    str(lpl)
-=======
-#' @param x A character or a factor vector or a data.frame. 
-#' @param dic A string (vector) naming eurostat dictionary or dictionaries.
-#'  If \code{NULL} (default) dictionry names taken from column names of 
-#'  the data.frame.
-#' @param code For data.frames names of the column for which also code columns
-#'   should be retained. The suffix "_code" is added to code column names.  
-#' @param eu_order Logical. Should Eurostat ordering used for label levels. 
-#'   Affects only factors.
-#' @param lang A character, code for language. Available are "en" (default), 
-#'        "fr" and "de".
-#' @export
-#' @author Janne Huovari <janne.huovari@@ptt.fi>
-#' @return a vector or a data.frame.
-#' @examples
-#'  \dontrun{
-#'    lp <- get_eurostat("nama_aux_lp")
-#'    lpl <- label_eurostat(lp)
-#'    str(lpl)
-#'    lpl_order <- label_eurostat(lp, eu_order = TRUE)
-#'    lpl_code <- label_eurostat(lp, code = "unit")
-#'    label_eurostat_vars(names(lp))
-#'    label_eurostat_tables("nama_aux_lp")
->>>>>>> 0a2e28c0fa0ba9750e49739c9a7998fef19be93d
 #'  }
 #'  
 
-merge_with_geodata <- function(data,geocol="geo",res=60){
+merge_with_geodata <- function(data,geocolumn="geo",resolution=60){
   load(url(paste0("http://data.okf.fi/ropengov/avoindata/eurostat_geodata/rdata/NUTS_2013_",
-                  res,
+                  resolution,
                   "M_SH_DF.RData")))
   map.df <- get(paste0("NUTS_2013_",res,"M_SH_DF"))
-  d <- merge(data,map.df,by.x=geocol,by="NUTS_ID",all.x=TRUE)
+  d <- merge(data,map.df,by.x=geocolumn,by="NUTS_ID",all.x=TRUE)
   d <- d[order(d$order),] 
   return(d)
 }
