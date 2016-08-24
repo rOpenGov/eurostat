0ace561c96b7422dd72165de8f9115fec745c973
Markus Kainu <markuskainu@gmail.com>
2016-08-24 15:32:39 +0300

    classInt added as Imports

--- R/cut_to_classes.R
+++ R/cut_to_classes.R
@@ -27,7 +27,7 @@ cut_to_classes <- function(x, n=5,style="jenks",manual=FALSE,manual_breaks = NUL
                                      dig.lab=5)))
   } else {
     levs <- as.data.frame(levels(cut(x, 
-                                     breaks=data.frame(classIntervals(x,n=n,style=style)[2])[,1],
+                                     breaks=data.frame(classInt::classIntervals(x,n=n,style=style)[2])[,1],
                                      include.lowest=T,
                                      dig.lab=5)))
   } 
@@ -55,7 +55,7 @@ cut_to_classes <- function(x, n=5,style="jenks",manual=FALSE,manual_breaks = NUL
              dig.lab=5, labels = labs)
     rm(manual_breaks)
   } else {
-    y <- cut(x, breaks = data.frame(classIntervals(x,n=n,method=method)[2])[,1],
+    y <- cut(x, breaks = data.frame(classInt::classIntervals(x,n=n,method=method)[2])[,1],
              include.lowest=T,
              dig.lab=5, labels = labs)
   }
