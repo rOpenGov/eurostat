3e5ca5fd36028b3cfd7021f8b3d4d56b8ac0fad7
Markus Kainu <muuankarski@kapsi.fi>
2018-05-12 21:43:19 +0300

    filter.sf removed from imported functions as not part of namespace:sf, lets see

--- R/firstlib.R
+++ R/firstlib.R
@@ -8,7 +8,6 @@
 #' @importFrom readr read_tsv
 #' @importFrom readr col_character
 #' @importFrom sf st_as_sf
-#' @importFrom sf filter.sf
 #' @importFrom sp merge
 #' @importFrom stringi stri_match_first_regex
 #' @importFrom stringr str_replace_all
