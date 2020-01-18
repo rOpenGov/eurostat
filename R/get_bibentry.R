2dbd2990ebffd605a84156694e6e1262c1f2ea5b
antagomir <leo.lahti@iki.fi>
2020-01-18 21:33:06 +0200

    moved imports to firstlib.R

--- R/get_bibentry.R
+++ R/get_bibentry.R
@@ -11,7 +11,6 @@
 #' @export
 #' @author Daniel Antal, Przemyslaw Biecek
 #' @return a bibentry, Bibtex or Biblatex object.
-#' @import lubridate dmy, year, month, day
 #' @examples
 #'  \dontrun{
 #'    my_bibliography <- get_bibentry (
