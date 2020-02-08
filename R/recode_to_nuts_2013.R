885f6747ff1596a8739c31cbb6810e42059f6062
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-02-08 22:29:00 +0100

    recoding is renamed and tested.

--- R/recode_to_nuts_2013.R
+++ R/recode_to_nuts_2013.R
@@ -15,27 +15,31 @@
 #' Furthermore, when the official name of the region changed, it will use
 #' the new name (if the otherwise the region boundary did not change.)
 #' If not called before, the function will use the helper function
-#'  \code{\link{check_nuts_2013}} and  \code{\link{harmonize_geo_code}}
+#' \code{\link{harmonize_geo_code}}
 #' @importFrom dplyr mutate filter rename arrange case_when
 #' @importFrom dplyr left_join inner_join anti_join
-#' @importFrom tidyselect all_of
 #' @examples
-#'  \dontrun{
-#'   eurostat::tgs00026 %>%
-#'      check_nuts2013() %>%
-#'      harmonize_geo_code() %>%
-#'      recode_to_nuts_2013() 
-#'      
-#'  #If check_nuts2013() is not called, the function will call it.    
-#'   eurostat::tgs00026 %>%
-#'      recode_to_nuts_2013()    
-#'  }
+#' test_regional_codes <- data.frame ( 
+#'   geo = c("FRB", "FRE", "UKN02", "IE022", "FR243", "FRB03"),
+#'   time = c(rep(as.Date ("2014-01-01"), 5), as.Date("2015-01-01")), 
+#'   values = c(1:6), 
+#'   control = c("Changed from NUTS2 to NUTS1", 
+#'               "New region NUTS2016 only", 
+#'               "Discontinued region NUTS2013", 
+#'               "Boundary shift NUTS2013", 
+#'               "Recoded in NUTS2013", 
+#'               "Recoded in NUTS2016"
+#'   )) 
+#'  
+#'  test_regional_codes %>%
+#'   harmonize_geo_code () %>%
+#'    recode_to_nuts_2013()
 #' @export
  
 recode_to_nuts_2013 <- function (dat) {
   
   . <- nuts_level <- geo <- code13 <- code16 <- time <- name <- NULL
-  type <- nuts_correspondence <- regional_changes_2016 <- NULL
+  type  <- NULL
 
   regional_changes_2016 <- load_package_data(dataset = "regional_changes_2016")
   nuts_correspondence <- load_package_data(dataset = "nuts_correspondence")
@@ -50,17 +54,19 @@ recode_to_nuts_2013 <- function (dat) {
   
   tmp <- tmp %>%
     mutate ( geo = case_when (
-      !is.na(geo)                   ~ geo,
-      change     == "not in the EU" ~ geo,
-      TRUE ~ code13
+      geo    == code13                       ~ geo,
+      change == "not in EU - not controlled" ~ geo,
+      TRUE   ~ code13
     ))
   
   if ( any (is.na(tmp$geo)) ) {
-    warning ( "The following regions have no geo labels:", 
+    warning ( "The following regions have no NUTS2013 geo labels: ", 
               tmp %>%
-                filter ( is.na(geo) && (nuts2016 = TRUE) ) %>%
-                select (geo) %>%
-                as.character() )
+                filter ( is.na(geo) & (nuts2013 = TRUE) ) %>%
+                select (code16) %>%
+                unlist() %>%
+                unique() %>% 
+                paste(., collapse = ", "), "." )
     
   }
   
@@ -74,12 +80,13 @@ recode_to_nuts_2013 <- function (dat) {
   
   regions_with_other_names <- tmp %>% 
     anti_join ( regions_by_nuts2013_names, 
-                    by = tidyselect::all_of(names(tmp)) )
+                    by = names(tmp) )
   
   rbind ( regions_by_nuts2013_names,
           regions_with_other_names ) %>%
     arrange ( time, geo, code16 ) %>%
     left_join ( nuts_correspondence, 
-                by = c("code13", "code16", "nuts_level", "change", "name"))
+                by = c("code13", "code16", "nuts_level",
+                       "change", "name", "resolution"))
   
 }
