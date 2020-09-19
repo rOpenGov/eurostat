2faef994bb0617e0b2df40308883f0352cbbf9bb
Leo <leo.lahti@iki.fi>
2020-09-19 11:58:24 +0300

    sync

--- R/search_eurostat.R
+++ R/search_eurostat.R
@@ -35,7 +35,7 @@ search_eurostat <- function(pattern, type = "dataset", fixed = TRUE) {
   # Check if you have access to ec.europe.eu. 
   if (!check_access_to_data()){
     message("You have no access to ec.europe.eu. 
-Please check your connection and/or review your proxy settings")
+      Please check your connection and/or review your proxy settings")
   } else {
   
   set_eurostat_toc()
