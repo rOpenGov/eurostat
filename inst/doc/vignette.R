ea4f9a8f036638865d83a19aa64e482eb0d5e696
antagomir <leo.lahti@iki.fi>
2020-11-18 11:59:03 +0200

    ok

---
+++ inst/doc/vignette.R
@@ -0,0 +1,15 @@
+## ----setup, include=FALSE-----------------------------------------------------
+# Global options
+library(knitr)
+# opts_chunk$set(fig.path="fig/")
+
+## ----install, eval=FALSE------------------------------------------------------
+#  install.packages("eurostat")
+
+## ----install2, eval=FALSE-----------------------------------------------------
+#  library(devtools)
+#  install_github("ropengov/eurostat")
+
+## ---- echo=FALSE--------------------------------------------------------------
+library(eurostat)
+
