52e480480da050a70f389572699f31a78054573f
Leo <leo.lahti@iki.fi>
2021-05-12 11:29:32 +0300

    ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
 #/usr/bin/R CMD BATCH document.R
 ~/bin/R-4.0.3/bin/R CMD build ../../ # --no-build-vignettes
-~/bin/R-4.0.3/bin/R CMD check --as-cran eurostat_3.7.4.tar.gz # --no-build-vignettes
-~/bin/R-4.0.3/bin/R CMD INSTALL eurostat_3.7.4.tar.gz
+~/bin/R-4.0.3/bin/R CMD check --as-cran eurostat_3.7.5.tar.gz # --no-build-vignettes
+~/bin/R-4.0.3/bin/R CMD INSTALL eurostat_3.7.5.tar.gz
