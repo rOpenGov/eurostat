61ae62cbf07fcb2f5bd262a001c1babcb4c292c5
Pyry Kantanen <pyry.kantanen@gmail.com>
2026-04-27 17:24:25 +0300

    Fix for warning message related to expected and observed column counts
    Warning message:
    One or more parsing issues, call `problems()` on your data frame for details, e.g.:
      dat <- vroom(...)
      problems(dat)
    
    # A tibble: 1 × 5
        row   col expected  actual    file
      <int> <int> <chr>     <chr>     <chr>
    1  9663     9 8 columns 9 columns ""

--- tests/testthat/test_01_bibliography.R
+++ tests/testthat/test_01_bibliography.R
@@ -2,17 +2,21 @@ test_that("Bibentry gives correct results", {
   skip_on_cran()
   skip_if_offline()
   expect_equal(
-    class(get_bibentry(
-      code = c("sts_inpr_a", "nama_10_gdp"),
-      keywords = list(
-        c("production", "industry"),
-        c("GDP")
-      ),
-      format = "Biblatex"
-    )),
+    class(
+      suppressWarnings( #necessary due to occasional extra fields in eurostat TOC files
+        get_bibentry(
+          code = c("sts_inpr_a", "nama_10_gdp"),
+          keywords = list(
+            c("production", "industry"),
+            c("GDP")
+          ),
+          format = "Biblatex"
+        )
+      )
+    ),
     "Bibtex"
   )
-  
+
   expect_equal(
     class(
       suppressWarnings(
@@ -23,11 +27,11 @@ test_that("Bibentry gives correct results", {
       ))),
     "Bibtex"
   )
-  
+
   expect_error(
     get_bibentry(code = 123456)
   )
-  
+
   expect_error(
     get_bibentry(
       code = c("sts_inpr_a"),
@@ -35,21 +39,21 @@ test_that("Bibentry gives correct results", {
       keywords = "production"
     )
   )
-  
+
   # None of the codes not found
   expect_warning(
     get_bibentry(
       code = "sts_inpr_b"
     )
   )
-  
+
   # Some of the codes not found
   expect_warning(
     get_bibentry(
       code = c("sts_inpr_b", "sts_inpr_a")
     )
   )
-  
+
   expect_warning(
     get_bibentry(
       code = c("sts_inpr_a", "nama_10_gdp"),
@@ -60,6 +64,6 @@ test_that("Bibentry gives correct results", {
       format = "character"
     )
   )
-  
-  
+
+
 })
