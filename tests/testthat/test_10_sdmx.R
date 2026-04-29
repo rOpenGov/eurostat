c24c3a67c4007e2aa1f394b43cdcd9d68323a022
Pyry Kantanen <pyry.kantanen@gmail.com>
2026-04-29 10:54:13 +0300

    Replace dataset ds-059327 with another COMEXT dataset
    "The dataset "ds-059327" has been discontinued since 05/03/2026." (http://data.europa.eu/88u/dataset/1o8g23hraiu3f4kz6vpnqw)

--- tests/testthat/test_10_sdmx.R
+++ tests/testthat/test_10_sdmx.R
@@ -1,28 +1,32 @@
 test_that("get_eurostat_sdmx works", {
   skip_on_cran()
   skip_if_offline()
-  
-  prodcom <- get_eurostat_sdmx(id = "ds-059327", agency = "eurostat_comext", 
-                               filters = 
+
+  # Dimension order for DS-059328:
+  # Reporter.partner.product.flow.period.indicators
+  # although it seems that omitting period is no problem?
+
+  prodcom <- get_eurostat_sdmx(id = "DS-059328", agency = "eurostat_comext",
+                               filters =
                                  list(
-                                   FREQ = c("A"),
-                                   REPORTER = "FR",
-                                   PARTNER = "US",
-                                   PRODUCT = c("291"),
-                                   FLOW = "2",
+                                   FREQ = c("A"), # Annual
+                                   REPORTER = "FR", # France
+                                   PARTNER = "US", # United States
+                                   PRODUCT = c("122"), # 122-Food and beverages / Processed / Mainly for household consumption
+                                   FLOW = "2", # 1-IMPORT, 2-EXPORT
                                    INDICATORS = "VALUE_EUR"), verbose = FALSE)
-  
+
   expect_equal(unique(prodcom$freq), "A")
-    
-  prodcom2 <- get_eurostat_sdmx(id = "ds-059327", agency = "eurostat_comext", 
-                             filters = 
+
+  prodcom2 <- get_eurostat_sdmx(id = "ds-059328", agency = "eurostat_comext",
+                             filters =
                                list(
                                  FREQ = c("M"),
                                  REPORTER = "FR",
                                  PARTNER = "US",
-                                 PRODUCT = c("291"),
-                                 FLOW = "2",
+                                 PRODUCT = c("310"), # 310-Fuels and lubricants / Primary
+                                 FLOW = "1",
                                  INDICATORS = "VALUE_EUR"), verbose = FALSE, use.data.table = TRUE)
-  
+
   expect_equal(unique(prodcom2$freq), "M")
 })
